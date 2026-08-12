8a3df4762cdc860ffeba7edf65aac7bda53fef6d
iamneongo <tripvang123@gmail.com>
2026-08-12 13:39:57 +0700

    Connect Mividoor admin entry points to CMS

---
+++ public/wp-login.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="vi">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="refresh" content="0;url=https://noithattuantai.com/wp-login.php">
+    <title>Mividoor CMS</title>
+  </head>
+  <body>
+    <p>Đang chuyển tới trang đăng nhập CMS...</p>
+    <script>window.location.replace("https://noithattuantai.com/wp-login.php");</script>
+  </body>
+</html>
